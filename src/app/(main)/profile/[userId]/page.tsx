8b2e289429d385f94b4208642b4b9554bf9ce3f3
Truong Quoc Bao (FPL HCM) <baotqps36791@fpt.edu.vn>
2024-11-04 18:14:47 +0700

    fix: fix ui home page

--- src/app/(main)/profile/[userId]/page.tsx
+++ src/app/(main)/profile/[userId]/page.tsx
@@ -1,51 +1,80 @@
-import ForYou from "@/components/post/ForYou"
-import { Button } from "@/components/ui/button"
-import { Main } from "next/document"
-import Image from "next/image"
+import PostEditor from '@/components/post/editor/PostEditor';
+import ForYou from '@/components/post/ForYou';
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
 
 const avatars = [
-    "https://i.pravatar.cc/150?img=1",
-    "https://i.pravatar.cc/150?img=2",
-    "https://i.pravatar.cc/150?img=3",
-    "https://i.pravatar.cc/150?img=4",
-    "https://i.pravatar.cc/150?img=5",
-    "https://i.pravatar.cc/150?img=6",
-]
+    'https://i.pravatar.cc/150?img=1',
+    'https://i.pravatar.cc/150?img=2',
+    'https://i.pravatar.cc/150?img=3',
+    'https://i.pravatar.cc/150?img=4',
+    'https://i.pravatar.cc/150?img=5',
+    'https://i.pravatar.cc/150?img=6',
+];
 
 const Page = async ({ params }: { params: { userId: string } }) => {
-
-    const { userId } = params
+    const { userId } = params;
     return (
-        <main>
+        <main className="flex-1">
             <div className="h-[348px] w-full">
-                <Image src="https://images.unsplash.com/photo-1661956600684-97d3a4320e45?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" alt="user profile" className="w-full h-full" width={1000} height={348} />
+                <Image
+                    src="https://images.unsplash.com/photo-1661956600684-97d3a4320e45?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+                    alt="user profile"
+                    className="w-full h-full object-cover rounded-lg"
+                    width={1000}
+                    height={348}
+                />
             </div>
             <div className="mt-8 flex justify-between">
                 <div className="flex gap-4">
                     <div className="w-[168px] h-[168px] rounded-full overflow-hidden">
-                        <Image src={"https://images.unsplash.com/photo-1661956600684-97d3a4320e45?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"} alt="user profile" className="w-full h-full object-cover" width={168} height={168} />
+                        <Image
+                            src={
+                                'https://images.unsplash.com/photo-1661956600684-97d3a4320e45?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
+                            }
+                            alt="user profile"
+                            className="w-full h-full object-cover"
+                            width={168}
+                            height={168}
+                        />
                     </div>
                     <div className="flex flex-col">
                         <h1 className="text-3xl font-medium">Quoc Bao</h1>
-                        <p className="mt-1 text-accent-foreground"> 1,1k followers . 1k following</p>
+                        <p className="mt-1 text-accent-foreground">
+                            {' '}
+                            1,1k followers . 1k following
+                        </p>
                         <div className="flex gap-3 mt-4 ml-6">
                             {avatars.map((avatar, index) => (
-                                <div key={index} className="w-[40px] h-[40px] rounded-full overflow-hidden -ml-6 p-1 bg-muted">
-                                    <Image src={avatar} alt="user profile" className="w-full h-full object-cover rounded-full" width={48} height={48} />
+                                <div
+                                    key={index}
+                                    className="w-[40px] h-[40px] rounded-full overflow-hidden -ml-6 p-1 bg-muted"
+                                >
+                                    <Image
+                                        src={avatar}
+                                        alt="user profile"
+                                        className="w-full h-full object-cover rounded-full"
+                                        width={48}
+                                        height={48}
+                                    />
                                 </div>
                             ))}
                         </div>
                     </div>
-
                 </div>
                 <div className="">
-                    <Button className="w-full"  > Follow</Button>
+                    <Button className="w-full"> Follow</Button>
                 </div>
             </div>
 
-            <ForYou />
+            <div className="mt-5">
+                <PostEditor />
+            </div>
+            <div className="mt-5">
+                <ForYou />
+            </div>
         </main>
-    )
-}
+    );
+};
 
-export default Page
+export default Page;
--- src/components/post/Post.tsx
+++ src/components/post/Post.tsx
@@ -1,58 +1,89 @@
-import { Ellipsis, Heart, MessageCircle, Share2, X } from "lucide-react"
-import Image from "next/image"
+import { Ellipsis, Heart, MessageCircle, Share2, X } from 'lucide-react';
+import Image from 'next/image';
 
 interface IPostProps {
-    name: string
-    avatar: string
-    medeia: string[] | string
-    content: string
-    hagtag: string | string[]
+    name: string;
+    avatar: string;
+    medeia: string[] | string;
+    content: string;
+    hagtag: string | string[];
 }
 const Post = ({ name, avatar, medeia, content, hagtag }: IPostProps) => {
     return (
         <div className="p-4 rounded-lg shadow">
             <div className="flex justify-between">
                 <div className="flex gap-3 items-center">
-                    <Image src={avatar} alt="User avatar" width={48} height={48} className="rounded-full object-cover overflow-hidden size-[48px]" />
+                    <Image
+                        src={avatar}
+                        alt="User avatar"
+                        width={500}
+                        height={500}
+                        className="rounded-full object-cover overflow-hidden size-[48px]"
+                    />
                     <div className="flex flex-col ">
                         <p className="font-medium">{name}</p>
                         <p className="text-muted-foreground">8h ago</p>
                     </div>
-
                 </div>
                 <div className="flex gap-3 items-center">
-                    <Ellipsis />
-                    <X />
+                    <div className="p-2 rounded-full hover:bg-muted">
+                        <Ellipsis />
+                    </div>
+                    <div className="p-2 rounded-full hover:bg-muted">
+                        <X />
+                    </div>
                 </div>
             </div>
             <div className="mt-3">
-                <p className="text-popover-foreground">
-                    {content}
-                </p>
+                <p className="text-popover-foreground">{content}</p>
             </div>
             <div className="mt-3">
                 <p className="text-popover-foreground">
-                    {Array.isArray(hagtag) && hagtag.map((hagtag, index) => (<span key={index} className="text-primary">{hagtag}</span>))}
+                    {Array.isArray(hagtag) &&
+                        hagtag.map((hagtag, index) => (
+                            <span key={index} className="text-primary">
+                                {hagtag}
+                            </span>
+                        ))}
                 </p>
             </div>
             <div className="mt-3">
-                {!Array.isArray(medeia) && <Image src={medeia} alt="User avatar" width={48} height={48} className="w-full h-full object-cover overflow-hidden max-h-[448px] rounded-lg" />}
-                {Array.isArray(medeia) && <div className="flex gap-2">{
-                    medeia.map((medeia, index) => (
-                        <Image src={medeia} alt="User avatar" width={48} height={48} key={index} className="w-full h-full object-cover overflow-hidden rounded-lg" />
-                    ))
-                }</div>}
+                {!Array.isArray(medeia) && (
+                    <Image
+                        src={medeia}
+                        alt="User avatar"
+                        width={48}
+                        height={48}
+                        className="w-full h-full object-cover overflow-hidden max-h-[448px] rounded-lg"
+                    />
+                )}
+                {Array.isArray(medeia) && (
+                    <div className="flex gap-2">
+                        {medeia.map((medeia, index) => (
+                            <Image
+                                src={medeia}
+                                alt="User avatar"
+                                width={48}
+                                height={48}
+                                key={index}
+                                className="w-full h-full object-cover overflow-hidden rounded-lg"
+                            />
+                        ))}
+                    </div>
+                )}
             </div>
 
             <div className="mt-2 py-3 flex justify-between ">
                 <div className="flex gap-3 items-center">
                     <Heart />
-                    <p className="text-popover-foreground"> You and 300 others</p>
+                    <p className="text-popover-foreground">
+                        {' '}
+                        You and 300 others
+                    </p>
                 </div>
                 <p className="text-popover-foreground"> 100 comments</p>
             </div>
             <div className="mt-2 py-2 grid grid-cols-3   px-4 border-t border-b border-muted">
-
                 <button className="flex gap-3 items-center justify-center w-full py-2 transition duration-200 rounded-lg hover:bg-muted ">
                     <Heart /> <p>Like</p>
                 </button>
@@ -64,7 +95,7 @@ const Post = ({ name, avatar, medeia, content, hagtag }: IPostProps) => {
                 </button>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Post
+export default Post;
